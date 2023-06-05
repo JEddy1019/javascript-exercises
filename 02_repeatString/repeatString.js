64eb4c0f2851d5fc6654aeb2d50724ce2ba9c284
JEddy1019 <back.up.plan.1019@gmail.com>
2023-06-05 12:35:39 -0400

    Complete Exercise 2
    Completed Exercise and passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,27 @@
-const repeatString = function() {
-
+let string,
+    num;
+const repeatString = function(string, num) {
+    let temp = '';
+    let x=0
+    // if(x<=-1){
+    //     temp = 'ERROR';
+    //     return temp;
+    // }else{
+    //     temp = '';
+    // for (x;x<num;x++){
+    //     temp += string;
+    // }
+    // return temp;
+    // }
+    for (x;x<num;x++){
+        if (num<0) break;
+        temp += string;
+    }
+    if (num<0){
+         temp = 'ERROR'
+    }
+    return temp;
+    
 };
 
 // Do not edit below this line
