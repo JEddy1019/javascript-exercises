f858aec49f831315ef906cf498e83b98423ac60d
JEddy1019 <back.up.plan.1019@gmail.com>
2023-06-05 15:19:37 -0400

    Complete Exercise 3
    Completed Exercise 3 and passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,26 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    let length = word.length,
+        x=0,
+        z = 0,
+        wordArray = [],
+        reversedArray = [],
+        outLength = length - 1,
+        reversedWord;
+
+    while(x<length){
+        wordArray[x] = word.substr(x, 1);
+         x++;
+    }
+    
+    for (let y = outLength; y>=0; y--) {
+        
+        reversedArray[y]= wordArray[z];
+        z++;
+    }
+
+    reversedWord = reversedArray.join('');
+    return reversedWord;
 
 };
 
