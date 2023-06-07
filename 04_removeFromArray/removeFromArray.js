34e6c138b8d7b41c4139036cc94d243e11e7c00c
JEddy1019 <back.up.plan.1019@gmail.com>
2023-06-07 18:35:10 -0400

    Complete Exercise 4
    Completed Exercise 4 and passed all of the tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elements) {
+    let lengthA = array.length,
+        newArray,
+        lengthE = arguments.length;
 
+    for (let x= 0; x<lengthE; x++){
+         for (let y = 0; y<lengthA; y++){
+            // if (typeof array[y] !== typeof elements[x]) continue;
+            if (array[y]===elements[x]){
+                array.splice(y,1);
+            }
+        
+            
+        }
+    }
+    newArray = array;
+    return newArray;
+    
 };
 
 // Do not edit below this line
