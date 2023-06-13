8f55817bec4dd3d2afa4de0a4f59b719e81b76f7
JEddy1019 <back.up.plan.1019@gmail.com>
2023-06-13 11:56:16 -0400

    Completed Exercise 5
    Completed Exercise 5 and passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+
+        let temp;
+        let sum = 0;
+        let firstType = typeof first;
+        let lastType = typeof last;
+
+        if (first>last){         
+            temp=first;
+            first=last;
+            last=temp;
+            }
+    
+        if (first < 0 || last < 0 || firstType != 'number' || lastType != 'number' ) {
+             return 'ERROR';
+            }
+
+        for (let x = first; x<=last; x++){
+            sum+=x;
+            }
+
+        return sum;
 
 };
 
