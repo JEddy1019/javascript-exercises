380c0c6f499653f3406e81953e38d71fd930b3ca
JEddy1019 <back.up.plan.1019@gmail.com>
2023-06-19 12:45:39 -0400

    Complete Exercise 4
    Completed Exercise 4 and passed all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,27 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let multipleOf4 = year % 4,
+    multipleOf400 = year % 400,
+    multipleOf100 = year % 100,
+    answer;
+
+if (!multipleOf4 && multipleOf100){
+    answer = true;
+}
+
+if (!multipleOf4 && !multipleOf100){
+    if (!multipleOf100 && !multipleOf400){
+        answer = true;
+    }else{
+        answer = false;
+    }
+    
+}
+
+if (multipleOf4){
+    answer = false;
+}
+
+return answer;
 
 };
 
