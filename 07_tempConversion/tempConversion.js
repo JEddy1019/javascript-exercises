b6ceea1241bca22319fb538fad37feefea98d6dc
JEddy1019 <back.up.plan.1019@gmail.com>
2023-06-19 13:22:39 -0400

    Complete Exercise 7
    Competed Exercise 7 and passed all the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+  let convert =  (temp - 32) * 5/9;
+  
+  return round(convert, 1);
+
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+  }
+
+  let convert =  temp *9/5 +32; 
+  
+  return round(convert, 1);
+
+
+
 };
 
 // Do not edit below this line
